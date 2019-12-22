69c887639bef19c3879c4e6f08b796e446a31ae9
Nguyen Ha <nynha0526@gmail.com>
2019-12-22 00:59:44 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,24 @@
-def hello_t
+list = ["ab", "cd", "ef"]
 
+def my_each(array)
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
 end
 
-# call your method here!
+my_each(list) {|i| puts "So I think #{i} is a piece of cake"}
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
+hello(list) {|i| "Hello #{i} what's up?"}
 
